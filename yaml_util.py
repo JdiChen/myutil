87056372c408e841691c9966fa213baaf4de6a27
Fang_Yue <48125566+JdiChen@users.noreply.github.com>
2022-02-16 09:36:25 +0800

    Update yaml_util.py

--- yaml_util.py
+++ yaml_util.py
@@ -7,6 +7,11 @@ def yaml_read(path):
         file_read = yaml.safe_load(f)
     return file_read
 
+def a(wordtext:str):
+    words = wordtext.strip().split(" ")
+    return set(words)
+if __name__ == '__main__':
+    print(a(" a a b c c "))
 
 if __name__ == '__main__':
     print(yaml_read('test.yaml'))
